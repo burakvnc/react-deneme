5823e7d794a36bfb7c9a1bfb3dbc7a892f81eaad
Burak Övence <ovence29@gmail.com>
2022-07-13 18:49:04 +0300

    test3

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import Logo from "./logo.svg"
 function App() {
   return <div className="App">
-    sa
+    {process.env.NODE_ENV === 'production' && (
     <img src={Logo} alt=""></img>
+    )}
   </div>;
 }
-
 export default App;
